b97e8078d45ff6632cf2c1910c6237c185413c31
Ramiro Murature <ramiromurature@Ramiros-MacBook-Air.local>
2026-05-04 20:21:19 -0300

    fix export config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { Suspense } from "react";
 import HomeContent from "./HomeContent";
 
+export const dynamic = "force-dynamic";
+
 export default function Page() {
   return (
     <Suspense fallback={<div />}>
